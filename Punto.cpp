940e50c7a32c1ef48e2a3086549fd60c2f416ebb
Manuel <31109774+Megalight09@users.noreply.github.com>
2017-09-22 15:52:06 -0400

    fix: Hilos no existentes de la clase Punto
    La clase thread no existe dentro del espacio de nombres std.

--- Punto.cpp
+++ Punto.cpp
@@ -1,31 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Punto.cpp
  * Author: Aybar
  * 
  * Created on September 14, 2017, 1:14 PM
  */
-
 #include "Punto.h"
 #include <fstream>
 #include <stdexcept>
 #include <string>
-#include <thread>
 using std::fstream;
 using std::string;
-using std::thread;
 Punto::Punto() {
     abscisa = 0;
     ordenada = 0;
     persist = std::vector<Punto>();
     try {
-        thread t(&Punto::leer, this);
-        t.join();
+		leer();
     } catch (string) {
 
     }
@@ -49,16 +39,14 @@ Punto Punto::buscar(const unsigned x) {
 
 bool Punto::alta(const int x, const int y) {
     persist.push_back(Punto(x, y));
-    thread t(&Punto::grabar, this);
-    t.join();
+	grabar();
     return true;
 
 }
 
 bool Punto::baja(const unsigned pos) {
     persist.erase(persist.begin() + pos);
-    thread t(&Punto::grabar, this);
-    t.join();
+	grabar();
     return true;
 
 }
